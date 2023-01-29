5dc1794049f7dff03677e2dff5c41c2a182e677a
qiushaocloud <qiushaocloud@126.com>
2023-01-29 13:49:27 +0800

    update create dir sh

--- create_nfs_dir.sh
+++ create_nfs_dir.sh
@@ -12,6 +12,11 @@ if [ ! $NFS_DIR ];then
 fi
 
 if [ $IS_DOCKER_ENV == "yes" ] && [ $ROOT_SHARE_DIR_TMP ];then
+    if [ ! -d $ROOT_SHARE_DIR_TMP$NFS_DIR ]; then
+        echo "docker dir $ROOT_SHARE_DIR_TMP$NFS_DIR have already existed"
+        exit 0
+    fi
+
     echo "create docker dir $ROOT_SHARE_DIR_TMP$NFS_DIR"
     mkdir -p $ROOT_SHARE_DIR_TMP$NFS_DIR
 
@@ -21,6 +26,10 @@ if [ $IS_DOCKER_ENV == "yes" ] && [ $ROOT_SHARE_DIR_TMP ];then
     echo "chmod docker dir $ROOT_SHARE_DIR_TMP$NFS_DIR"
     chmod 777 $ROOT_SHARE_DIR_TMP$NFS_DIR
 else
+    if [ ! -d $NFS_DIR ]; then
+        echo "dir $NFS_DIR have already existed"
+        exit 0
+    fi
     echo "create dir $NFS_DIR"
     mkdir -p $NFS_DIR
 
